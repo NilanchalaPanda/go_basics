483d0050f4f9a800a437fe4e6099b0a7438ccb23
Nilanchal <110488337+NilanchalaPanda@users.noreply.github.com>
2025-08-16 11:50:47 +0530

    update: added mutation methods

--- struct.go
+++ struct.go
@@ -13,11 +13,18 @@ type user struct {
 }
 
 func (u user) outputUserDetails() {
-	fmt.Println("User Details:")
-	fmt.Println("First Name:", u.firstName)
-	fmt.Println("Last Name:", u.lastName)
-	fmt.Println("Date of Birth:", u.dateOfBirth)
-	fmt.Println("Account Created At:", u.createdAt.Format("02/01/2006 15:04:05"))
+	// fmt.Println("User Details:")
+	// fmt.Println("First Name:", u.firstName)
+	// fmt.Println("Last Name:", u.lastName)
+	// fmt.Println("Date of Birth:", u.dateOfBirth)
+	// fmt.Println("Account Created At:", u.createdAt.Format("02/01/2006 15:04:05"))
+	fmt.Println(u.firstName, u.lastName, u.dateOfBirth)
+}
+
+// Mutation Methods - You should pass the pointer address and not just 'u user'. That will create a new copy of user.
+func (u *user) clearUserName() {
+	u.firstName = ""
+	u.lastName = ""
 }
 
 func main() {
@@ -38,6 +45,9 @@ func main() {
 	// outputUserDetails(appUser)
 
 	appUser.outputUserDetails()
+	appUser.clearUserName()
+	fmt.Println("User name cleared.")
+	appUser.outputUserDetails() // This will show empty first and last names after clearing
 }
 
 // func outputUserDetails(u user) {
